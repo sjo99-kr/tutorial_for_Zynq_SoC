d3262ca05ce22f498e1a07a7852f2fd4b944ec59
CastleFirstKR <154411859+CastleFirstKR@users.noreply.github.com>
2024-07-08 17:37:21 +0900

    Update and rename axi_dma_example2.h to axi_dma_example_1-2.h

--- axi_dma_example/axi_dma_example_1-2.h
+++ axi_dma_example/axi_dma_example_1-2.h
@@ -1,3 +1,15 @@
+/******************************************************************************
+
+Tutorial Materials for Embedded Software using Vitis IDE
+
+Using Acumulator Custom Ip, Do AXI-DMA process
+
+made by Seongwon Jo,
+
+
+*******************************************************************************/
+
+// Library Linking
 #include "xaxidma.h"
 #include "xparameters.h"
 #include "sleep.h"
@@ -33,9 +45,11 @@ int main(){
 	Xil_DCacheFlushRange((u32)a,8*sizeof(u32)); // Dirty Data management in Data Cache
 	status = XAxiDma_SimpleTransfer(&myDma, (u32)b, 8*sizeof(u32),XAXIDMA_DEVICE_TO_DMA);
 	status = XAxiDma_SimpleTransfer(&myDma, (u32)a, 8*sizeof(u32),XAXIDMA_DMA_TO_DEVICE);//typecasting in C/C++
+// XAXIDMA_SIMPLETRANSFER CAN NOT SEND DATA IN SG(Scatter - Gatter) DMA ENGINE!!!
+	
 
-/*
 // Using this form for check DMA State Register
+/*
 	if(status != XST_SUCCESS){
 		print("DMA initialization failed\n");
 		return -1;
@@ -49,7 +63,7 @@ int main(){
     while(status != 1){
     	status = checkHalted(XPAR_AXI_DMA_0_BASEADDR,0x34);
     }
-    */
+*/
 	print("DMA transfer success..\n");
 	for(int i=0;i<8;i++)
 		xil_printf("%0x\n",b[i]);
@@ -59,5 +73,7 @@ int main(){
 u32 checkHalted(u32 baseAddress,u32 offset){
 	u32 status;
 	status = (XAxiDma_ReadReg(baseAddress,offset))&XAXIDMA_HALTED_MASK;
+	// XAXIDMA_HALTED_MASK -> DMA Channel is Halt
+	// if halt, status is set 0
 	return status;
 }
