e6b41305c2ce7d286676b89da576bb5a0e5e122f
CastleFirstKR <154411859+CastleFirstKR@users.noreply.github.com>
2024-07-08 16:57:55 +0900

    Update and rename axi_double_bram.v to axi_double_bram.h

--- axi_bram/axi_double_bram.h
+++ axi_bram/axi_double_bram.h
@@ -1,22 +1,15 @@
 /******************************************************************************
-* Copyright (C) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
-* SPDX-License-Identifier: MIT
-******************************************************************************/
-/*
- * helloworld.c: simple test application
- *
- * This application configures UART 16550 to baud rate 9600.
- * PS7 UART (Zynq) is not initialized by this application, since
- * bootrom/bsp configures it to baud rate 115200
- *
- * ------------------------------------------------
- * | UART TYPE   BAUD RATE                        |
- * ------------------------------------------------
- *   uartns550   9600
- *   uartlite    Configurable only in HW design
- *   ps7_uart    115200 (configured by bootrom/bsp)
- */
 
+Tutorial Materials for Embedded Software using Vitis IDE
+
+Using AXI_Bram controller Ip, WRITE DATA AND READ DATA FROM BRAM 
+
+made by Seongwon Jo,
+
+
+*******************************************************************************/
+
+// Library Linking
 #include <stdio.h>
 #include "platform.h"
 #include "xil_printf.h"
@@ -24,39 +17,47 @@
 #include "xbram.h"
 #include <xbram_hw.h>
 
+// Set base addr of BRAMs (we set Two brams in PL)
 #define BRAM_CTRL_0_BASE XPAR_XBRAM_0_BASEADDR
 #define BRAM_CTRL_1_BASE XPAR_XBRAM_1_BASEADDR
 
 int main()
 {
+    // u32 means unsigned 32 bits 
     u32 wdata32, rdata32;
     u32 i;
 
+    // initialize our platform , I use Zynq 7020 
     init_platform();
 
-    xil_printf("Hello World\n\r");
     xil_printf("AXI_DOUBLE_BRAM ACCESS TEST!! \r\n");
-    // bram 0 write process
+    
+    // for bram 0, write data process
     wdata32 = 0;
     for(i=0; i<100; i++){
         XBram_WriteReg(BRAM_CTRL_0_BASE, i*4, wdata32);
+        // xbram_writereg(base addr, offset, data)
         wdata32 = wdata32 +2;
     }
     xil_printf("BRAM 0 WRITING PROCESS DONE \r\n");
-    // bram 1 write process
+    
+    // for bram 0, write data process
     wdata32 = 100;
     for(i=0; i<100; i++){
         XBram_WriteReg(BRAM_CTRL_1_BASE, i*4, wdata32);
         wdata32 = wdata32 +1;
-
     }
+    
     xil_printf("BRAM 1 WRITTING PROCESS DONE \r\n");
 
     xil_printf("\n\n BRAM 0 READING PROCESS START \r\n");
-    // bram 0 read process
+    
+    // for bram 0, read data process
     rdata32 = 0;
     for(i=0; i<100; i++){
         rdata32 = XBram_ReadReg(BRAM_CTRL_0_BASE, i*4);
+        //xbram_readreg(base addr, offset)
+        // we set our bram data width 32-bit
         if(i<10){
             xil_printf("READ VALUE FROM BRAM 0 : %d \n", rdata32);
         }
@@ -65,7 +66,8 @@ int main()
 
 
     xil_printf("\n\n BRAM 1 READING PROCESS START \r\n");
-    // bram 1 read process
+    
+    // for bram 1, read data process
     rdata32 = 0;
     for(i=0; i<100; i++){
         rdata32 = XBram_ReadReg(BRAM_CTRL_1_BASE, i*4);
@@ -73,6 +75,7 @@ int main()
             xil_printf("READ VALUE FROM BRAM 1 : %d \n", rdata32);
         }
     }
+    
     xil_printf("BRAM 1 READING PROCESS DONE \r\n");
 
 
