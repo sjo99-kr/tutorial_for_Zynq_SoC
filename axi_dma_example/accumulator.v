bf9631076685e873bfb8b63ddc2c882ff57d271f
CastleFirstKR <154411859+CastleFirstKR@users.noreply.github.com>
2024-07-08 17:06:35 +0900

    Update accumulator.v

--- axi_dma_example/accumulator.v
+++ axi_dma_example/accumulator.v
@@ -1,25 +1,16 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/07/2024 01:02:11 PM
-// Design Name: 
-// Module Name: accumulator
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+/******************************************************************************
+
+Tutorial Materials for Embedded Software using Vitis IDE
+
+Using AXI_Bram controller Ip, WRITE DATA AND READ DATA FROM BRAM 
+
+made by Seongwon Jo,
 
 
+*******************************************************************************/
+
+// Accumulator Custom IP in PL
 module accumulator(
     input i_clk,
     input i_rst,
@@ -29,6 +20,8 @@ module accumulator(
     
     output reg [31:0] o_data,
     output reg o_data_valid,
+
+    //INTERRUPT SIGNAL
     output reg o_intr
     );
     reg [4:0] counter;
@@ -40,6 +33,9 @@ module accumulator(
             counter <= 1;
         end
         else begin
+            // I_data_valid signal comes from AXI-Slave singal(s_axis_valid)
+            // AXI_DMA data comes s_axis ports. 
+            // if we sent 8 datas from DDR, i_data_valid set up for 8 cycles based on PL-Clock Cycle (100MHz)
             if(i_data_valid)begin
                 o_data <= o_data + i_data;
                 o_data_valid <= 1;
@@ -62,9 +58,16 @@ module accumulator(
         end
         else begin
             if( counter % 8 == 0)begin
+                // INTERRUPT SIGNAL O_intr
+                // turn valid when data sent is over
+                // only valid when we get 8 datas each time. 
                 o_intr <= 1;
             end
             else begin
+                // initialize o_intr
+                // PL-Clock Cycles cant not be over 250 MHz,
+                // But PS-clock Cycles (CPU or DDR) is over than 500MHz.
+                // so, Just turn on for one cycle is enought for PS interrupt.
                 o_intr <= 0;
             end
         end
