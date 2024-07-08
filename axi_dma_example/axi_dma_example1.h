34276b06fd53ef88727744ef50ddde2109070d8f
CastleFirstKR <154411859+CastleFirstKR@users.noreply.github.com>
2024-07-08 17:21:31 +0900

    Update axi_dma_example1.h

--- axi_dma_example/axi_dma_example1.h
+++ axi_dma_example/axi_dma_example1.h
@@ -1,21 +1,13 @@
 /******************************************************************************
-* Copyright (C) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
-* SPDX-License-Identifier: MIT
-******************************************************************************/
-/*
- * helloworld.c: simple test application
- *
- * This application configures UART 16550 to baud rate 9600.
- * PS7 UART (Zynq) is not initialized by this application, since
- * bootrom/bsp configures it to baud rate 115200
- *
- * ------------------------------------------------
- * | UART TYPE   BAUD RATE                        |
- * ------------------------------------------------
- *   uartns550   9600
- *   uartlite    Configurable only in HW design
- *   ps7_uart    115200 (configured by bootrom/bsp)
- */
+
+Tutorial Materials for Embedded Software using Vitis IDE
+
+Using Acumulator Custom Ip, Do AXI-DMA process
+
+made by Seongwon Jo,
+
+
+*******************************************************************************/
 
 #include <stdio.h>
 #include <xstatus.h>
