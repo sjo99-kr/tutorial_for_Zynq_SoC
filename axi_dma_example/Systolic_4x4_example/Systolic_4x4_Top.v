24f2ca31ae105012c9397d921239d014266da26b
CastleFirstKR <154411859+CastleFirstKR@users.noreply.github.com>
2024-07-09 13:40:24 +0900

    Update Systolic_4x4_Top.v

--- axi_dma_example/Systolic_4x4_example/Systolic_4x4_Top.v
+++ axi_dma_example/Systolic_4x4_example/Systolic_4x4_Top.v
@@ -1,7 +1,5 @@
 `timescale 1ns / 1ps
 
-
-
 module Systolic_4x4_Top(
     input axi_clk,
     input axi_rst,
@@ -19,17 +17,21 @@ module Systolic_4x4_Top(
     // interrupt
     output o_intr
     );
-    reg mmu_buffer_valid;
-    reg [4:0] valid_count;
+
     
     wire axis_prog_full;
     wire [31:0] col1, col2, col3, col4;
     wire [31:0] row1, row2, row3, row4;
     wire buffer_valid;
     
-    wire [127:0] mmu_out;
+    wire [31:0] mmu_out1, mmu_out2, mmu_out3, mmu_out4;
+    wire mmu_valid1, mmu_valid2, mmu_valid3, mmu_valid4;
     
     reg mmu_valid;
+    
+    wire [31:0] accum_out;
+    wire accum_out_valid;
+    
     assign s_axis_ready = !axis_prog_full;
     
     Matrix_Buffer MB( 
@@ -52,22 +54,45 @@ module Systolic_4x4_Top(
         .col1_in(col1), .col2_in(col2), .col3_in(col3), .col4_in(col4),
         .row1_in(row1), .row2_in(row2), .row3_in(row3), .row4_in(row4),
         
-        .out_data(mmu_out)
+        .o_data1(mmu_out1), .o_data2(mmu_out2), .o_data3(mmu_out3), .o_data4(mmu_out4),
+        .o_valid1(mmu_valid1), .o_valid2(mmu_valid2), .o_valid3(mmu_valid3), .o_valid4(mmu_valid4)
     );
     
-
-    
-    mmu_buffer(
-        .wr_rst_busy(),
-        .rd_rst_busy(),
-        .s_aclk(axi_clk),
-        .s_aresetn(axi_rst),
-        .s_axis_tvalid(
     
     
-    )
     
-    
-
+    accumulate_buffer accum_buffer(
+        .i_clk(axi_clk),
+        .i_rst(axi_rst),
+        .i_data1(mmu_out1),
+        .i_data2(mmu_out2),
+        .i_data3(mmu_out3),
+        .i_data4(mmu_out4),
         
+        .i_valid1(mmu_valid1),
+        .i_valid2(mmu_valid2),
+        .i_valid3(mmu_valid3),
+        .i_valid4(mmu_valid4),
+        
+        .o_data(accum_out),
+        .o_valid(accum_out_valid)
+    );
+    
+    fifo_generator_0 Stream_Buffer(
+        .wr_rst_busy(),        // output wire wr_rst_busy
+        .rd_rst_busy(),        // output wire rd_rst_busy
+        .s_aclk(axi_clk),                  // input wire s_aclk
+        .s_aresetn(axi_rst),            // input wire s_aresetn
+        .s_axis_tvalid(accum_out_valid),    // input wire s_axis_tvalid
+        .s_axis_tready(),    // output wire s_axis_tready
+        .s_axis_tdata(accum_out),     
+       
+        .m_axis_tvalid(m_axis_valid),    
+        .m_axis_tready(m_axis_ready),   
+        .m_axis_tdata(m_axis_data),      
+        .axis_prog_full(axis_prog_full)
+    
+    
+    );
+         
 endmodule
